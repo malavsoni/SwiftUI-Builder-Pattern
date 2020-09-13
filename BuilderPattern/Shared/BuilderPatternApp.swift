c1a8340399c2ab53c3c3e154c39af321bc84e604
Malav Soni <malav@live.com>
2020-09-13 15:15:35 +0530

    Basic Structure

---
+++ BuilderPattern/Shared/BuilderPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuilderPatternApp.swift
+//  Shared
+//
+//  Created by Malav Soni on 13/09/20.
+//
+
+import SwiftUI
+
+@main
+struct BuilderPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
